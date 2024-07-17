7cca3f9e5c71922572974036be57c262153351e6
froostang <froostang@gmail.com>
2024-07-17 00:00:34 -0400

    add jwt shared middleware code

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module github.com/froostang/retail-therapy/api
 
 go 1.21.1
+
+require github.com/froostang/retail-therapy/shared v0.0.0-20240717035514-24e8f0ff4a51
--- shared/middleware/jwt.go
+++ shared/middleware/jwt.go
@@ -1,5 +1,57 @@
-package jwt
+package middleware
 
-func NewJWTMiddleware()
+import (
+	"net/http"
+	"strings"
 
-// TODO: JWT middleware implementation here
+	"github.com/golang-jwt/jwt"
+)
+
+func JWTMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := extractTokenFromHeader(r)
+		if tokenString == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := validateToken(tokenString)
+		if err != nil {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		if token.Valid {
+			next.ServeHTTP(w, r)
+		} else {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+		}
+	})
+}
+
+func extractTokenFromHeader(r *http.Request) string {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return ""
+	}
+
+	tokenParts := strings.Split(authHeader, " ")
+	if len(tokenParts) != 2 || strings.ToLower(tokenParts[0]) != "bearer" {
+		return ""
+	}
+
+	return tokenParts[1]
+}
+
+func validateToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validate the token signing method here
+		return []byte("your-secret-key"), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
